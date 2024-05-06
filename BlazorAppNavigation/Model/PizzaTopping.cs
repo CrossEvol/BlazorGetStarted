b24c600f96b96a7457e2bcdcb697f0cd09204a08
创圣大天使Evol <296662402@qq.com>
2024-05-06 21:23:42 +0800

    Add BlazorAppNavigation

---
+++ BlazorAppNavigation/Model/PizzaTopping.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppNavigation;
+
+public class PizzaTopping
+{
+    public Topping Topping { get; set; }
+
+    public int ToppingId { get; set; }
+
+    public int PizzaId { get; set; }
+}
