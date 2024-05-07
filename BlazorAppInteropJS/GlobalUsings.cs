9262fdf8fd2ba7b8908f71957df8bb74070fca40
创圣大天使Evol <296662402@qq.com>
2024-05-07 16:44:50 +0800

    Import BlazorAppInteropJS

---
+++ BlazorAppInteropJS/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using BlazorAppInteropJS;
+global using BlazorAppInteropJS.Data;
+global using BlazorAppInteropJS.Extensions;
+global using BlazorAppInteropJS.Models;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+
+global using System.ComponentModel.DataAnnotations;
+global using System.Text.Json.Serialization;
